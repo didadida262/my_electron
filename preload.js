002a7e1a477860927638e03faa88d98c0c1684b4
hhvcg <719713496@qq.com>
2023-07-06 23:54:52 +0800

    add: 1.1

--- preload.js
+++ preload.js
@@ -1,3 +1,14 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron,
+    ping: () => ipcRenderer.invoke('ping')
+    // 除函数之外，我们也可以暴露变量
+  })
+
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
