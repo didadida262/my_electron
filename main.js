c93f5ca2d2dea410b61c1f8a7e133762c8ce016a
hhvcg <719713496@qq.com>
2023-07-07 23:02:58 +0800

    add: 窗口控制

--- main.js
+++ main.js
@@ -3,16 +3,22 @@ const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-      x: 1000,
-      y: 400,
+      // x: 1000,
+      // y: 400,
+      // maxHeight: 800,
+      // maxWidth: 800,
+      // minHeight: 400,
+      // minWidht: 400,
       width: 800,
       height: 600,
+      resizable: false,
       show: false,
       webPreferences: {
         preload: path.join(__dirname, 'preload.js')
       }
     })
     win.loadFile('index.html')
+    // 处理白屏
     win.on('ready-to-show', () => {
       win.show()
     })
