bd80a8691b0f29329988364656228f24605169d0
hhvcg <719713496@qq.com>
2023-07-07 23:00:07 +0800

    add: 处理白屏

--- main.js
+++ main.js
@@ -3,14 +3,20 @@ const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
+      x: 1000,
+      y: 400,
       width: 800,
       height: 600,
+      show: false,
       webPreferences: {
         preload: path.join(__dirname, 'preload.js')
       }
     })
-  
     win.loadFile('index.html')
+    win.on('ready-to-show', () => {
+      win.show()
+    })
+  
   }
 
   app.whenReady().then(() => {
